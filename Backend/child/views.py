2b9257e9c21cb61e9f2bb2715cd699aa67bbd74a
JRButoto <johnbutoto@gmail.com>
2024-06-05 23:31:10 +0300

    changed get summary to get list and set only as response

--- Backend/child/views.py
+++ Backend/child/views.py
@@ -30,30 +30,54 @@ class ChildConsultationVisitView(viewsets.ModelViewSet):
     #permission_classes=[permissions.IsAuthenticated]
 
 
+# @api_view(['GET'])
+# def getChildSummary(request):
+#     children_data = Child.objects.all()
+#     children_data_Serializer = ChildSummarySerializer(children_data, many = True)
+    
+#     # Extract weight and height from Child_visit objects
+#     children_visits_data = Child_visit.objects.all()
+#     children_visit_data_Serializer = ChildVisitSummarySerializer(children_visits_data, many = True)
+
+#     # Extract ages to add to 'other' part of the response
+#     children_ages = [
+#         {
+#             'child_name': child.child_name,
+#             'age': ChildSummarySerializer(child).data['age']
+#         }
+#         for child in children_data
+#     ]
+    
+#     # Construct the response data
+#     response_data = {
+#         'children': children_data_Serializer.data,
+#         'children_visits': children_visit_data_Serializer.data,
+        
+#     }
+
+#     return Response(response_data)
+
 @api_view(['GET'])
 def getChildSummary(request):
     children_data = Child.objects.all()
-    children_data_Serializer = ChildSummarySerializer(children_data, many = True)
-    
-    # Extract weight and height from Child_visit objects
-    children_visits_data = Child_visit.objects.all()
-    children_visit_data_Serializer = ChildVisitSummarySerializer(children_visits_data, many = True)
+    response_data = []
 
-    # Extract ages to add to 'other' part of the response
-    children_ages = [
-        {
-            'child_name': child.child_name,
-            'age': ChildSummarySerializer(child).data['age']
-        }
-        for child in children_data
-    ]
-    
-    # Construct the response data
-    response_data = {
-        'children': children_data_Serializer.data,
-        'children_visits': children_visit_data_Serializer.data,
-        
-    }
+    for child in children_data:
+        child_serializer = ChildSummarySerializer(child)
+        visits = Child_visit.objects.filter(child=child)
+        visit_serializer = ChildVisitSummarySerializer(visits, many=True)
+
+        for visit in visit_serializer.data:
+            combined_data = {
+                'child_name': child_serializer.data['child_name'],
+                'child_gender': child_serializer.data['child_gender'],
+                'mother_name': child_serializer.data['mother_name'],
+                'age': child_serializer.data['age'],
+                'weight_grams': visit['weight_grams'],
+                'height': visit['height'],
+                'date': visit['date'],
+            }
+            response_data.append(combined_data)
 
     return Response(response_data)
 
