3d0527bae1881c6dd32308d8cd132489135c3f58
JRButoto <johnbutoto@gmail.com>
2024-06-06 14:24:15 +0300

    mother summary added url and id

--- Backend/mother/serializers.py
+++ Backend/mother/serializers.py
@@ -32,10 +32,6 @@ class MotherSerializer(serializers.HyperlinkedModelSerializer):
         ]
 
 
-# class MotherVisitSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model= Mother_visit
-#         fields= "__all__"
 
 class MotherVisitSerializer(serializers.HyperlinkedModelSerializer):
     mother_name = serializers.CharField()
@@ -99,37 +95,15 @@ class MotherVisitSerializer(serializers.HyperlinkedModelSerializer):
         return mother_visit
 
 
-# class MotherSummarySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Mother
-#         fields = ['mother_name', 'mother_age']
 
-class MotherSummarySerializer(serializers.ModelSerializer):
+class MotherSummarySerializer(serializers.HyperlinkedModelSerializer):
 
-    age = serializers.SerializerMethodField()
     class Meta:
         model = Mother
-        fields = ['id', 'mother_name', 'age', 'partner_name']
+        fields = ['url','id', 'mother_name', 'mother_age', 'partner_name']
     
-    def get_age(self, obj):
-        today = date.today()
-        # Calculate the difference in years
-        year_difference = today.year - obj.date_of_birth.year
-        # Calculate the difference in months
-        month_difference = today.month - obj.date_of_birth.month
-        # Calculate the difference in days
-        day_difference = today.day - obj.date_of_birth.day
-        
-        # Adjust year and month differences if needed
-        if day_difference < 0:
-            month_difference -= 1
-        if month_difference < 0:
-            year_difference -= 1
-            month_difference += 12
-        
-        return f"{year_difference} years, {month_difference} months"
 
-class MotherVisitSummarySerializer(serializers.ModelSerializer):
+class MotherVisitSummarySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Mother_visit
         fields = ['visit_number', 'visit_date']
--- Backend/mother/views.py
+++ Backend/mother/views.py
@@ -21,20 +21,57 @@ class MotherVisitViewSet(viewsets.ModelViewSet):
     #permission_classes=[permissions.IsAuthenticated]
 
 
+# @api_view(['GET'])
+# def getMotherSummary(request):
+#     Mother_data = Mother.objects.all()
+#     Mother_data_Serializer = MotherSummarySerializer(Mother_data, many = True)
+    
+#     # Extract weight and height from Child_visit objects
+#     Mother_visits_data = Mother_visit.objects.all()
+#     Mother_visit_data_Serializer = MotherVisitSummarySerializer(Mother_visits_data, many = True)
+    
+#     # Construct the response data
+#     response_data = {
+#         'Mother': Mother_data_Serializer.data,
+#         'Mother_visits': Mother_visit_data_Serializer.data
+#     }
+
+#     return Response(response_data)
+
 @api_view(['GET'])
 def getMotherSummary(request):
     Mother_data = Mother.objects.all()
-    Mother_data_Serializer = MotherSummarySerializer(Mother_data, many = True)
-    
-    # Extract weight and height from Child_visit objects
-    Mother_visits_data = Mother_visit.objects.all()
-    Mother_visit_data_Serializer = MotherVisitSummarySerializer(Mother_visits_data, many = True)
-    
-    # Construct the response data
-    response_data = {
-        'Mother': Mother_data_Serializer.data,
-        'Mother_visits': Mother_visit_data_Serializer.data
-    }
+    response_data = []
+
+    for mother in Mother_data:
+        mother_serializer = MotherSummarySerializer(mother, context={'request': request})
+        latest_visit = Mother_visit.objects.filter(mother=mother).order_by('-visit_date').first()
+        if latest_visit:
+            visit_serializer = MotherVisitSummarySerializer(latest_visit,context={'request': request})
+
+            combined_data = {
+                'url':mother_serializer.data['url'],
+                'id':mother_serializer.data['id'],
+                'mother_name': mother_serializer.data['mother_name'],
+                'mother_age': mother_serializer.data['mother_age'],
+                'partner_name': mother_serializer.data['partner_name'],
+                'visit_date': visit_serializer.data['visit_date'],
+                'visit_number': visit_serializer.data['visit_number']
+
+            }
+            response_data.append(combined_data)
+        else:
+            combined_data = {
+                'url':mother_serializer.data['url'],
+                'id':mother_serializer.data['id'],
+                'mother_name': mother_serializer.data['mother_name'],
+                'mother_age': mother_serializer.data['mother_age'],
+                'partner_name': mother_serializer.data['partner_name'],
+                'visit_date': None,
+                'visit_number': None
+
+            }
+            response_data.append(combined_data)
 
     return Response(response_data)
 
