7037f7e400afcfedcb3349579624c42d1598877e
JRButoto <johnbutoto@gmail.com>
2024-06-14 14:30:25 +0300

    fixed environment problem

--- Backend/mother/views.py
+++ Backend/mother/views.py
@@ -21,23 +21,6 @@ class MotherVisitViewSet(viewsets.ModelViewSet):
     #permission_classes=[permissions.IsAuthenticated]
 
 
-# @api_view(['GET'])
-# def getMotherSummary(request):
-#     Mother_data = Mother.objects.all()
-#     Mother_data_Serializer = MotherSummarySerializer(Mother_data, many = True)
-    
-#     # Extract weight and height from Child_visit objects
-#     Mother_visits_data = Mother_visit.objects.all()
-#     Mother_visit_data_Serializer = MotherVisitSummarySerializer(Mother_visits_data, many = True)
-    
-#     # Construct the response data
-#     response_data = {
-#         'Mother': Mother_data_Serializer.data,
-#         'Mother_visits': Mother_visit_data_Serializer.data
-#     }
-
-#     return Response(response_data)
-
 @api_view(['GET'])
 def getMotherSummary(request):
     Mother_data = Mother.objects.all()
