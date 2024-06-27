7280a53ebcd78f77dceda48e09f1bfdbda44202d
Deogratias Nyamhanga <deogratias287@gmail.com>
2024-06-27 17:53:07 +0300

    wowza

--- Backend/child/urls.py
+++ Backend/child/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from .views import getChildSummary, childStatistics, AggregatedDataSummaryView
+from .views import getChildSummary, childStatistics, AggregatedDataSummaryView, Analytics, AggregatedDataSummaryView1, AggregatedDataSummaryView2
 from .views import getChildSummary, childStatistics, get_child_nutrition_recomendations
 
 app_name = 'child'
@@ -10,7 +10,10 @@ urlpatterns = [
 
     path('getChildSummary/', getChildSummary, name='getChildSummary'),
     path('childStatistics/', childStatistics, name='childStatistics'),
-    path('aggregated-data-summary/', AggregatedDataSummaryView.as_view(), name='aggregated-data-summary'),
+    path('ReportMonthly/', AggregatedDataSummaryView.as_view(), name='aggregated-data-summary'),
+    path('ReportQuartely/', AggregatedDataSummaryView1.as_view(), name='aggregated-data-summary'),
+    path('ReportAnnualy/', AggregatedDataSummaryView2.as_view(), name='aggregated-data-summary'),
+    path('Analytics/', Analytics.as_view(), name='aggregated-data-summary'),
     path('get_child_nutrition_recomendations/', get_child_nutrition_recomendations, name='get_child_nutrition_recomendations'),
  
 ]
