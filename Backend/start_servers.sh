8c5e4dfb61c538a4f9140a91061b3b2cdf5b0271
JRButoto <johnbutoto@gmail.com>
2024-07-03 12:58:42 +0300

    changed docker file

--- Backend/start_servers.sh
+++ Backend/start_servers.sh
@@ -4,52 +4,52 @@
 # source "/home/godson/Projects/python environments/rasa/bin/activate"
 
 # Activate the virtual environment
-source /code/app/venv/bin/activate
+# source /code/app/venv/bin/activate
 
 # Store server PIDs
-rasa_pid=""
-action_pid=""
-python_pid=""
+backend_pid=""
+# action_pid=""
+# python_pid=""
 
-kill -9 $(lsof -t -i:5005)
-kill -9 $(lsof -t -i:5055)
-kill -9 $(lsof -t -i:3100)
-kill -9 $(lsof -t -i:3200)
+kill -9 $(lsof -t -i:8800)
+# kill -9 $(lsof -t -i:5055)
+# kill -9 $(lsof -t -i:3100)
+# kill -9 $(lsof -t -i:3200)
 
 # Change to the /code/app directory
-cd /code/app
+cd /code
 
-# Start Rasa server with CORS enabled
-rasa run --enable-api --cors "*" &
-rasa_pid=$!
+# # Start Rasa server with CORS enabled
+# rasa run --enable-api --cors "*" &
+# rasa_pid=$!
 
-# Start action server
-rasa run actions &
-action_pid=$!
+# # Start action server
+# rasa run actions &
+# action_pid=$!
+
+# # Start Python server (replace with your specific command)
+# python app.py &
+# python_pid=$!
 
 # Start Python server (replace with your specific command)
-python app.py &
-python_pid=$!
-
-# Start Python server (replace with your specific command)
-python sms.py &
-python_pid=$!
+python manage.py runserver &
+backend_pid=$!
 
 # Wait for servers to start
 sleep 5
 
 echo "All servers are now running!"
-echo "Rasa server: http://localhost:5005"
-echo "Action server: http://localhost:5055"
-echo "Python app server: http://localhost:3100"
-echo "Python sms server: http://localhost:3200"
+echo "Backend server: http://localhost:8800"
+# echo "Action server: http://localhost:5055"
+# echo "Python app server: http://localhost:3100"
+# echo "Python sms server: http://localhost:3200"
 
 
 # Stop function for graceful shutdown
 stop_servers() {
   echo "Stopping servers..."
-  kill -SIGTERM $rasa_pid $action_pid $python_pid
-  wait $rasa_pid $action_pid $python_pid  # Wait for processes to exit
+  kill -SIGTERM $backend_pid #$action_pid $python_pid
+  wait $backend_pid # $action_pid $python_pid  # Wait for processes to exit
 
   # Perform cleanup tasks (e.g., delete temporary files)
   echo "Performing cleanup tasks..."
